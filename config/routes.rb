7967bbbd7fd621d9a242eaf7776e9ffeed263743
Issei Kuzumaki <kuzumaki-issei-sk@ynu.jp>
2016-01-06 03:25:20 +0900

    signup layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
   root to: "landing_page#home"
-
   get 'landing_page/help'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
