4af9b6f38e1cae1fe725414f27cbbd2637fd72a0
Issei Kuzumaki <kuzumaki-issei-sk@ynu.jp>
2016-01-06 00:22:46 +0900

    name and email shoud not be too long

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,4 +18,14 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "      "
     assert_not @user.valid?
   end
+
+  test "name should not be too long(<= 50 characters)" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long(<= 255 characters)" do
+    @user.email = "a" *244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
