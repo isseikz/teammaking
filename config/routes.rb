8265beae35b073056395f2ec809cdc814539489d
Issei Kuzumaki <kuzumaki-issei-sk@ynu.jp>
2016-01-06 01:55:46 +0900

    simple layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: "landing_page#home"
+
+  get 'landing_page/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
